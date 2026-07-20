f8207c6c26f8f1e8e7f63dd38bcdbfcc21c7af5c
Sandesh <sandeshojha70@gmail.com>
2026-07-20 20:46:11 +0530

    feat: initialize networking
    Signed-off-by: Sandesh <sandeshojha70@gmail.com>

--- cmd/bootstrapper/main.go
+++ cmd/bootstrapper/main.go
@@ -4,32 +4,31 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"syscall"
 	"strconv"
+	"syscall"
 
+	"github.com/vishvananda/netlink"
 	"golang.org/x/sys/unix"
-
 )
 
 func main() {
 
-	if (len(os.Args) < 2) {
+	if len(os.Args) < 2 {
 		panic("Expected 'run' or 'child'")
 	}
 
 	switch os.Args[1] {
-		case "run":
-			parent()
+	case "run":
+		parent()
 
-		case "child":
-			child()
+	case "child":
+		child()
 
-		default:
-			panic("an error occured...")
+	default:
+		panic("an error occured...")
 
 	}
 }
-
 
 func parent() {
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
@@ -51,8 +50,6 @@ func parent() {
 
 	cgroupPath := "/sys/fs/cgroup/bootstrapper"
 
-
-
 	if err := os.Mkdir(cgroupPath, 0755); err != nil && !os.IsExist(err) {
 		log.Fatalf("Error creating the folder: %v", err)
 	}
@@ -73,15 +70,37 @@ func parent() {
 		log.Fatalf("Failed to join cgroup: %v", err)
 	}
 
+	// define the bridge
+	bridge := &netlink.Bridge{LinkAttrs: netlink.LinkAttrs{Name: "br-boot"}}
+
+	// create the link
+	if err := netlink.LinkAdd(bridge); err != nil {
+		log.Fatalf("Error creating link: %v", err)
+	}
+
+	// parse the ip address
+	addr, err := netlink.ParseAddr("10.0.0.1/24")
+	if err != nil {
+		log.Fatalf("Error parsing the ip address: %v", err)
+	}
+
+	// attach the ip to the bridge
+	if err := netlink.AddrAdd(bridge, addr); err != nil {
+		log.Fatalf("Error attaching the ip address: %v", err)
+	}
+
+	if err := netlink.LinkSetUp(bridge); err != nil {
+		log.Fatalf("Error setting up the link: %v", err)
+	}
+
 	if err := cmd.Wait(); err != nil {
 		log.Fatal("Wait failed")
 	}
-
 }
 
-func child(){
+func child() {
 
-	// declare that the mount is private 
+	// declare that the mount is private
 	if err := unix.Mount("", "/", "", unix.MS_PRIVATE|unix.MS_REC, ""); err != nil {
 		log.Fatalf("Private mount failed: %v", err)
 	}
@@ -90,24 +109,23 @@ func child(){
 	if err := unix.Sethostname([]byte("myContainer")); err != nil {
 		log.Fatalf("Setting hostname failed: %v", err)
 	}
-		
+
 	if err := unix.Chroot("./rootfs"); err != nil {
 		log.Fatalf("Chroot failed: %v", err)
 	}
 
-
 	if err := os.Chdir("/"); err != nil {
 		log.Fatalf("Chdir failed: %v", err)
 	}
-		// isolate mountspace
-		// src, target, filesystemtype, mountflag, data
+	// isolate mountspace
+	// src, target, filesystemtype, mountflag, data
 	if err := unix.Mount("proc", "/proc", "proc", 0, ""); err != nil {
 		log.Fatalf("Proc mount falied: %v", err)
 
 	}
 
 	//isolate sys mount
-	if err := unix.Mount("sysfs", "/sys", "/sysfs", 0, ""); err != nil {
+	if err := unix.Mount("sysfs", "/sys", "sysfs", 0, ""); err != nil {
 		log.Fatalf("Sys mount failed: %v", err)
 	}
 
@@ -117,7 +135,5 @@ func child(){
 	if err := unix.Exec("/bin/sh", []string{"/bin/sh"}, os.Environ()); err != nil {
 		log.Fatalf("Exec failed: %v", err)
 	}
-
-
 
 }
